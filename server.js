6db19238ed841024a6a85bfa63defc51f5c486b8
Alexander Vadeboncoeur <asvadeboncoeur@gmail.com>
2021-06-02 18:32:53 -0500

    started workout model and created skeleton for api routes

--- server.js
+++ server.js
@@ -13,9 +13,8 @@ app.use(express.json());
 
 app.use(express.static('public'));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/budget', {
-  useNewUrlParser: true,
-//   useFindAndModify: false
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout', {
+  useNewUrlParser: true
 });
 
 
