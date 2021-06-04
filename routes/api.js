fdb931f2993ebfac784eb34715d8fd8b64777f3d
Alexander Vadeboncoeur <asvadeboncoeur@gmail.com>
2021-06-04 01:46:10 -0500

    created workout totalDuration display on homepage functioning

--- routes/api.js
+++ routes/api.js
@@ -1,15 +1,33 @@
 const router = require('express').Router();
 const Workout = require('../models/workout');
-const path = require('path');
 
-router.get('/api/workouts', async (req, res) => {
-    try {
-        const workoutData = await Workout.find({})
+// router.get('/api/workouts', async (req, res) => {
+//     try {
+//         const workoutData = await Workout.find({})
+
+//         res.json(workoutData);
+//     } catch (err) {
+//         res.status(400).json(err);
+//     }
+// });
+
+router.get('/api/workouts', (req, res) => {
+    Workout.aggregate([
+        {
+            $addFields: {
+                totalDuration: { $sum: '$exercises.duration' }
+            }
+        }
+    ])
+    .then(workoutData => {
+        console.log(workoutData);
+        
 
         res.json(workoutData);
-    } catch (err) {
+    })
+    .catch(err => {
         res.status(400).json(err);
-    }
+    });
 });
 
 router.post('/api/workouts', ({ body }, res) => {
@@ -38,6 +56,11 @@ router.put('/api/workouts/:id', ({ body, params }, res) => {
         res.status(400).json(err);
     });
 });
+
+
+// router.delete('/api/workouts/:id', ({ params }, res) => {
+
+// });
 
 
 
