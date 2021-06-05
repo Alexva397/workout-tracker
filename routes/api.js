2017eeda59b46b6f3a8e708fea48dddebbf2b7a8
Alexander Vadeboncoeur <asvadeboncoeur@gmail.com>
2021-06-05 14:05:31 -0500

    created rokouts/range route w/ a limit set to 7

--- routes/api.js
+++ routes/api.js
@@ -21,7 +21,6 @@ router.get('/api/workouts', (req, res) => {
     ])
     .then(workoutData => {
         console.log(workoutData);
-        
 
         res.json(workoutData);
     })
@@ -30,12 +29,12 @@ router.get('/api/workouts', (req, res) => {
     });
 });
 
+
 router.post('/api/workouts', ({ body }, res) => {
     Workout.create(body)
     .then(workoutData => {
         console.log(workoutData);
         
-
         res.json(workoutData);
     })
     .catch(err => {
@@ -49,13 +48,35 @@ router.put('/api/workouts/:id', ({ body, params }, res) => {
     .then(workoutData => {
         console.log(workoutData);
         
-
         res.json(workoutData);
     })
     .catch(err => {
         res.status(400).json(err);
     });
 });
+
+
+router.get('/api/workouts/range', (req, res) => {
+    Workout.aggregate([
+        {
+            $addFields: {
+                totalDuration: { $sum: '$exercises.duration' }
+            }
+        }
+    ])
+    .sort({ _id: -1 })
+    .limit(7)
+    .then(workoutRangeData => {
+        console.log(workoutRangeData);
+
+        res.json(workoutRangeData);
+    })
+    .catch(err => {
+        res.status(400).json(err);
+    });
+});
+
+
 
 
 // router.delete('/api/workouts/:id', ({ params }, res) => {
